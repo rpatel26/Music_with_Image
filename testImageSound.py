c878d651632406509d4bd3d14f7a290afba6b957
rdpatel26 <rdp2606@gmail.com>
2018-07-29 12:00:30 -0700

    argument parser added

--- testImageSound.py
+++ testImageSound.py
@@ -1,15 +1,25 @@
 from imageSound import ImageSound
+import argparse
 
 
 if ( __name__ == '__main__' ):
-  file1 = './sources/image1.jpg'
-  file2 = './sources/image2.jpg'
-  
-  test1 = ImageSound(file1)
-  test1.playImage()
-  test1.playFilteredImage()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f', "--file", 
+		help = "Fill path of the image source")
+	args = parser.parse_args()
 
-  # print()
-  # test2 = ImageSound(file2)
-  # test2.playImage()
-  # test2.playFilteredImage()
+	if args.file is None:
+		file1 = './sources/image1.jpg'
+	else:
+		file1 = args.file
+
+	file2 = './sources/image2.jpg'
+
+	test1 = ImageSound(file1)
+	test1.playImage()
+	test1.playFilteredImage()
+
+	# print()
+	# test2 = ImageSound(file2)
+	# test2.playImage()
+	# test2.playFilteredImage()
